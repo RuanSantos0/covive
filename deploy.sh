117eca7252d2bfd519908f1da90cb5fe5e01b04e
Ruan Santos <rugondim@gmail.com>
2020-05-05 13:48:21 -0300

    make it better

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/RuanSantos0/covive.git master:gh-pages
+cd -
