bcda0a5a721ce7f68d5369b411a82915fdf092af
Ruan Santos <rugondim@gmail.com>
2020-05-05 13:20:18 -0300

    deploy

---
+++ precache-manifest.f14ea1ab3fd514d1a6090a5fda365d2f.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0ce8c8c361cbe18ee92",
+    "url": "/css/app.ff12dfe2.css"
+  },
+  {
+    "revision": "a0ce8c8c361cbe18ee92",
+    "url": "/js/app.3011aa0f.js"
+  },
+  {
+    "revision": "27c747772f53ab9e482f",
+    "url": "/js/chunk-52e4de86.ab3eff60.js"
+  },
+  {
+    "revision": "78bbd2d90a7461df6d32",
+    "url": "/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "78bbd2d90a7461df6d32",
+    "url": "/js/chunk-ebdaf33a.6a3943b1.js"
+  },
+  {
+    "revision": "c10ba4fd9903abe9b171",
+    "url": "/js/chunk-vendors.bc31c4cb.js"
+  },
+  {
+    "revision": "3173efe5a9d939890b472c15c0c7620a",
+    "url": "/img/covid_bg.3173efe5.svg"
+  },
+  {
+    "revision": "2159a79d096a32c63f0cedb2baa930a5",
+    "url": "/img/covid_white.2159a79d.svg"
+  },
+  {
+    "revision": "46747b84702969b3810bad8ad3cc0e59",
+    "url": "/img/covive2.46747b84.svg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "135daf3144366c7fcf1e52a72c2dfb93",
+    "url": "/img/ifce-logo.135daf31.png"
+  },
+  {
+    "revision": "d1ca1dc97b4eedb3bbddc5d35d04a545",
+    "url": "/img/logo-covive.d1ca1dc9.svg"
+  },
+  {
+    "revision": "e97d933ae78425474e83dd64954dc52a",
+    "url": "/img/search.e97d933a.svg"
+  },
+  {
+    "revision": "faa1b50065add29021b0086aa75e85b8",
+    "url": "/img/ms-logo.faa1b500.png"
+  },
+  {
+    "revision": "0fed5e6d9ba293f2c7643d6a24845613",
+    "url": "/img/covive.0fed5e6d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7b03e437038ae5442c0f6d6341fa46ce",
+    "url": "/img/no_covid.7b03e437.svg"
+  },
+  {
+    "revision": "a2fdbc9bef505e495421413db89db64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b03e437038ae5442c0f6d6341fa46ce",
+    "url": "/img.svg"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  }
+];
